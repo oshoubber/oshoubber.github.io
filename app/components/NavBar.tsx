9ee8324803c2bcf4fdcf9909138decf00d753356
Osama Shoubber <oshoubber@gmail.com>
2023-08-23 01:22:42 -0700

    fixed navbar once and for all

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,8 +3,12 @@ import { Navbar, NavbarContent, NavbarItem, Link } from '@nextui-org/react';
 
 export default function App() {
   return (
-    <Navbar className="bg-black w-full justify-between">
-      <NavbarContent className="sm:flex gap-4 p-4" justify="start">
+    <Navbar
+      className="bg-black w-full flex justify-between"
+      isBlurred
+      maxWidth="full"
+    >
+      <NavbarContent className="w-full gap-4 p-4">
         <NavbarItem>
           <Link className="hover:text-yellow-500" color="foreground" href="#">
             Projects
@@ -16,7 +20,7 @@ export default function App() {
           </Link>
         </NavbarItem>
       </NavbarContent>
-      <NavbarContent className="p-4" justify="end">
+      <NavbarContent className="w-full p-4" justify="end">
         <NavbarItem className="hover:text-yellow-500">
           <Link className="hover:text-yellow-500" color="foreground" href="#">
             Contact
