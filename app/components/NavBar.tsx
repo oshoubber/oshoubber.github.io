c540e1f155237a72316980aa9857d3813fc2cf0e
Osama Shoubber <oshoubber@gmail.com>
2023-08-23 00:54:47 -0700

    Update NavBar.tsx

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -11,23 +11,23 @@ import {
 export default function App() {
   return (
     <Navbar className="bg-black w-full justify-between">
-      <NavbarContent className="sm:flex gap-4 p-4">
+      <NavbarContent className="sm:flex gap-4 p-4" justify="start">
         <NavbarItem>
           <Link className="hover:text-yellow-500" color="foreground" href="#">
             Projects
           </Link>
         </NavbarItem>
         <NavbarItem className="hover:text-yellow-500">
-          <Link href="#" aria-current="page">
+          <Link className="hover:text-yellow-500" color="foreground" href="#">
             About
           </Link>
         </NavbarItem>
       </NavbarContent>
-      <NavbarContent className="p-4">
+      <NavbarContent className="p-4" justify="end">
         <NavbarItem className="hover:text-yellow-500">
-          <Button as={Link} color="primary" href="#" variant="flat">
+          <Link className="hover:text-yellow-500" color="foreground" href="#">
             Contact
-          </Button>
+          </Link>
         </NavbarItem>
       </NavbarContent>
     </Navbar>
