695cf8e50c13b54b709e0b0c3d17628df6913253
Osama Shoubber <oshoubber@gmail.com>
2023-08-24 01:45:05 -0700

    animated theme switch

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex flex-col min-h-screen pb-12">
+    <main className="flex flex-col min-h-screen pb-12 transition-colors duration-500 dark:bg-background bg-light-background">
       <NavBar />
       <div className="pt-24 px-12">
         <h1 className="text-5xl animate-fadeInUp">
