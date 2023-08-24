77b4a255b0c370e6020eb225419f76f47843d240
Osama Shoubber <oshoubber@gmail.com>
2023-08-24 00:35:22 -0700

    google site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="vbjmjHTBehnlJ5KwipXJL_m2wx8wtz29cbGcpBbOI5Q"
+        />
+      </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
